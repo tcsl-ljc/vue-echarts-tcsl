ca27b46c54156a33b5b0650211bbf06d1c9dae4e
dingyumei <916646428@qq.com>
2019-06-03 18:00:13 +0800

    update: column

--- src/myPlugin/echarts-template/column/column.vue
+++ src/myPlugin/echarts-template/column/column.vue
@@ -8,9 +8,12 @@ import minXinChart from '../mixin';
 import {Option as columnblue} from './column-blue.js';
 import {Option as columnorange} from './column-orange.js';
 import {Option as columnpurple}  from './column-purple.js';
+import _ from 'lodash';
+import {dataformatCommon} from '../dataformat';
 export default {
   mixins: [minXinChart],
   props: {
+    chartData: Object,
     data: {
       default: function () {
         return [
@@ -36,8 +39,17 @@ export default {
   },
   methods: {
     resolveData (Option) {
-      //
-      Option.series[0].data = this.data;
+      // Option.series[0].data = this.data;
+      let _data = dataformatCommon(this.chartData);
+      Option.xAxis.data = _data.xAxisData;
+      let temp = _.cloneDeep(Option.series[0]);
+      Option.series = [];
+      for (let i = 0; i < _data.seriesData.length; i++) {
+        let ser = _.cloneDeep(temp);
+        ser.data = _data.seriesData[i].data;
+        ser.name = _data.seriesData[i].name;
+        Option.series.push(ser);
+      }
     }
   }
 };
